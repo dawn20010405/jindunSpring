424f61e6608f6c7ee83ef63a49344247e1bf942d
丁刚 <@shark_chili>
2020-12-21 23:49:17 +0800

    关系实体

---
+++ newSpringWeb/src/main/java/com/demo/pojo/Intrecord.java
@@ -0,0 +1,73 @@
+package com.demo.pojo;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * Description: <br/>
+ * date: 2020/12/21 23:19<br/>
+ *
+ * @author Administrator<br />
+ * @since JDK 1.8
+ */
+@Entity
+public class Intrecord {
+    private Integer irid;
+    private Integer iresult;
+    private String iremark;
+    private Interview invw;
+
+    @Id
+    @Column(name = "irid", nullable = false)
+    public Integer getIrid() {
+        return irid;
+    }
+
+    public void setIrid(Integer irid) {
+        this.irid = irid;
+    }
+
+    @Basic
+    @Column(name = "iresult", nullable = false)
+    public Integer getIresult() {
+        return iresult;
+    }
+
+    public void setIresult(Integer iresult) {
+        this.iresult = iresult;
+    }
+
+    @Basic
+    @Column(name = "iremark", nullable = true, length = 255)
+    public String getIremark() {
+        return iremark;
+    }
+
+    public void setIremark(String iremark) {
+        this.iremark = iremark;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Intrecord intrecord = (Intrecord) o;
+        return Objects.equals(irid, intrecord.irid) &&
+                Objects.equals(iresult, intrecord.iresult) &&
+                Objects.equals(iremark, intrecord.iremark);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(irid, iresult, iremark);
+    }
+
+    @OneToOne(mappedBy = "inrd")
+    public Interview getInvw() {
+        return invw;
+    }
+
+    public void setInvw(Interview invw) {
+        this.invw = invw;
+    }
+}
---
+++ newSpringWeb/src/main/java/com/demo/pojo/Position.java
@@ -0,0 +1,85 @@
+package com.demo.pojo;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * Description: <br/>
+ * date: 2020/12/21 22:56<br/>
+ *
+ * @author Administrator<br />
+ * @since JDK 1.8
+ */
+@Entity
+public class Position {
+    private Integer pid;
+    private String poname;
+    private Integer ptype;
+    private Dept dept;
+    private List<Resume> rsid;
+
+    @Id
+    @Column(name = "pid", nullable = false)
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    @Basic
+    @Column(name = "poname", nullable = false, length = 255)
+    public String getPoname() {
+        return poname;
+    }
+
+    public void setPoname(String poname) {
+        this.poname = poname;
+    }
+
+    @Basic
+    @Column(name = "ptype", nullable = false)
+    public Integer getPtype() {
+        return ptype;
+    }
+
+    public void setPtype(Integer ptype) {
+        this.ptype = ptype;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return Objects.equals(pid, position.pid) &&
+                Objects.equals(poname, position.poname) &&
+                Objects.equals(ptype, position.ptype);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pid, poname, ptype);
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "did", referencedColumnName = "did")
+    public Dept getDept() {
+        return dept;
+    }
+
+    public void setDept(Dept dept) {
+        this.dept = dept;
+    }
+
+    @OneToMany(mappedBy = "posi")
+    public List<Resume> getRsid() {
+        return rsid;
+    }
+
+    public void setRsid(List<Resume> rsid) {
+        this.rsid = rsid;
+    }
+}
