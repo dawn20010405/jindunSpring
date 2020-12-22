a0cbe38c435e954c6ac8901f67d7d0561f1818d5
Length <918721751@qq.com>
2020-12-22 15:26:16 +0800

    2020 12 22 15:26

---
+++ newSpringWeb/src/main/java/com/demo/Controller/B.java
@@ -0,0 +1,17 @@
+package com.demo.Controller;/**
+ * @Auther: Length
+ * @Date: 2020/12/22 15:24
+ * @Description:
+ */
+
+/**
+ *@description: 测试
+ *@author: larry
+ *@create: 2020-12-22 15:24
+ */
+public class B {
+
+    public void fun1(){
+        System.out.println("B.class 2020-12-22 15:25");
+    }
+}
